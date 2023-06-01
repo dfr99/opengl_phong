7aae5e23692b20892899b5fb45b90194456545fa
David Fraga Rodríguez <david.fraga.rodriguez@udc.es>
2023-06-01 14:07:56 +0200

    Add base code for Phong

---
+++ spinningcube_withlight_vs.glsl
@@ -0,0 +1,23 @@
+# version 130
+
+in vec3 v_pos;
+in vec3 v_normal;
+in vec2 v_tex;
+
+out vec3 frag_3Dpos;
+out vec3 vs_normal;
+out vec2 TexCoords;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+uniform mat3 normal_to_world;
+
+void main() {
+
+  frag_3Dpos = vec3(model * vec4(v_pos, 1.0));
+  vs_normal = normalize(normal_to_world * v_normal);
+
+  gl_Position = projection * view * model * vec4(v_pos, 1.0f);
+  TexCoords = v_tex;
+}
