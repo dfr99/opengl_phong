8ae122aba35111138c2115596fde7b43bb27c584
David Fraga Rodríguez <david.fraga.rodriguez@udc.es>
2023-06-03 12:00:56 +0200

    Initial version of spinning cube with phong

--- spinningcube_withlight.cpp
+++ spinningcube_withlight.cpp
@@ -19,11 +19,15 @@ int gl_height = 480;
 
 void glfw_window_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
-void render(double);
+void render(double, GLuint *vao);
+void obtenerNormales(GLfloat * normales,const GLfloat vertices[]);
 
 GLuint shader_program = 0; // shader program to set render pipeline
 GLuint vao = 0; // Vertext Array Object to set input data
-GLint model_location, view_location, proj_location; // Uniforms for transformation matrices
+GLint model_location, view_location, proj_location, normal_location; // Uniforms for transformation matrices
+GLint light_position_location, light_ambient_location, light_diffuse_location, light_specular_location; // Uniforms for light data
+GLint material_ambient_location, material_diffuse_location, material_specular_location, material_shininess_location; // Uniforms for material matrices
+GLint camera_pos_location;
 
 // Shader names
 const char *vertexFileName = "spinningcube_withlight_vs.glsl";
@@ -201,6 +205,8 @@ int main() {
      0.25f,  0.25f, -0.25f  // 3
   };
 
+  GLfloat normales[sizeof(float)* 108] = {};
+
 // Vertex Buffer Object (for vertex coordinates)
   GLuint vbo = 0;
   glGenBuffers(1, &vbo);
@@ -213,32 +219,63 @@ int main() {
   glEnableVertexAttribArray(0);
 
   // 1: vertex normals (x, y, z)
+  obtenerNormales(normales, vertex_positions);
+  GLuint normalesBuffer = 0;
+  glGenBuffers(1, &normalesBuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, normalesBuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(normales), normales, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+  glEnableVertexAttribArray(1);
+
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  // note that we update the lamp's position attribute's stride to reflect the updated buffer data
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+  glEnableVertexAttribArray(0);
 
   // Unbind vbo (it was conveniently registered by VertexAttribPointer)
   glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindBuffer(GL_ARRAY_BUFFER, 1);
 
   // Unbind vao
   glBindVertexArray(0);
+  glBindVertexArray(1);
 
   // Uniforms
+  
   // - Model matrix
-  // - View matrix
-  // - Projection matrix
-  // - Normal matrix: normal vectors from local to world coordinates
-  // - Camera position
-  // - Light data
-  // - Material data
   model_location = glGetUniformLocation(shader_program, "model");
+  
+  // - View matrix
   view_location = glGetUniformLocation(shader_program, "view");
+  
+  // - Projection matrix
   proj_location = glGetUniformLocation(shader_program, "projection");
-  // [...]
+
+  // - Normal matrix: normal vectors from local to world coordinates
+  normal_location = glGetUniformLocation(shader_program, "normal_to_world");
+  
+  // - Camera position
+  camera_pos_location = glGetUniformLocation(shader_program, "view_pos");
+  
+  // - Light data
+  light_position_location = glGetUniformLocation(shader_program, "light.position"); 
+  light_ambient_location = glGetUniformLocation(shader_program, "light.ambient"); 
+  light_diffuse_location = glGetUniformLocation(shader_program, "light.diffuse"); 
+  light_specular_location = glGetUniformLocation(shader_program, "light.specular");
+  
+  // - Material data
+  material_ambient_location = glGetUniformLocation(shader_program, "material.ambient"); 
+  material_diffuse_location = glGetUniformLocation(shader_program, "material.diffuse"); 
+  material_specular_location = glGetUniformLocation(shader_program, "material.specular"); 
+  material_shininess_location = glGetUniformLocation(shader_program, "material.shininess");
 
 // Render loop
   while(!glfwWindowShouldClose(window)) {
 
     processInput(window);
 
-    render(glfwGetTime());
+    render(glfwGetTime(), &vao);
 
     glfwSwapBuffers(window);
 
@@ -250,7 +287,7 @@ int main() {
   return 0;
 }
 
-void render(double currentTime) {
+void render(double currentTime, GLuint *vao) {
   float f = (float)currentTime * 0.3f;
 
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -258,24 +295,48 @@ void render(double currentTime) {
   glViewport(0, 0, gl_width, gl_height);
 
   glUseProgram(shader_program);
-  glBindVertexArray(vao);
+  glBindVertexArray(*vao);
 
   glm::mat4 model_matrix, view_matrix, proj_matrix;
+  glm::mat3 normal_matrix;
 
   model_matrix = glm::mat4(1.f);
   view_matrix = glm::lookAt(                 camera_pos,  // pos
                             glm::vec3(0.0f, 0.0f, 0.0f),  // target
                             glm::vec3(0.0f, 1.0f, 0.0f)); // up
 
-  // Moving cube
-  // model_matrix = glm::rotate(model_matrix,
-  //   [...]
-  //
+ model_matrix = glm::rotate(model_matrix,
+                          glm::radians((float)currentTime * 30.0f),
+                          glm::vec3(0.0f, 1.0f, 0.0f));
+
+  model_matrix = glm::rotate(model_matrix,
+                            glm::radians((float)currentTime * 81.0f),
+                            glm::vec3(1.0f, 0.0f, 0.0f));
+
   // Projection
-  // proj_matrix = glm::perspective(glm::radians(50.0f),
-  //   [...]
-  //
+  proj_matrix = glm::perspective(glm::radians(50.0f),
+                                 (float) gl_width / (float) gl_height,
+                                 0.1f, 1000.0f);
+  
+  glUniformMatrix4fv(view_location, 1, GL_FALSE, glm::value_ptr(view_matrix));
+  glUniformMatrix4fv(model_location, 1, GL_FALSE, glm::value_ptr(model_matrix));
+  glUniformMatrix4fv(proj_location, 1, GL_FALSE, glm::value_ptr(proj_matrix));
+  
   // Normal matrix: normal vectors to world coordinates
+  normal_matrix = glm::transpose(glm::inverse(glm::mat3(model_matrix)));
+  glUniformMatrix3fv(normal_location, 1, GL_FALSE, glm::value_ptr(normal_matrix));
+
+  glUniform3fv(light_position_location, 1, glm::value_ptr(light_pos));
+  glUniform3fv(light_ambient_location, 1, glm::value_ptr(light_ambient));
+  glUniform3fv(light_diffuse_location, 1, glm::value_ptr(light_diffuse));
+  glUniform3fv(light_specular_location, 1, glm::value_ptr(light_specular));
+
+  glUniform3fv(material_ambient_location, 1, glm::value_ptr(material_ambient));
+  glUniform3fv(material_diffuse_location, 1, glm::value_ptr(material_diffuse));
+  glUniform3fv(material_specular_location, 1, glm::value_ptr(material_specular));
+  glUniform1f(material_shininess_location, material_shininess);
+
+  glUniform3fv(camera_pos_location, 1, glm::value_ptr(camera_pos));
 
   glDrawArrays(GL_TRIANGLES, 0, 36);
 }
@@ -290,4 +351,33 @@ void glfw_window_size_callback(GLFWwindow* window, int width, int height) {
   gl_width = width;
   gl_height = height;
   printf("New viewport: (width: %d, height: %d)\n", width, height);
+}
+
+void obtenerNormales(GLfloat * normales,const GLfloat vertices[]){
+
+  for(int i = 0; i < 108; i+= 9){
+
+    GLfloat v1[] = {vertices[i], vertices[i+1], vertices[i+2]};
+    GLfloat v2[] = {vertices[i+3], vertices[i+4], vertices[i+5]};
+    GLfloat v3[] = {vertices[i+6], vertices[i+7], vertices[i+8]};
+
+    GLfloat U[] = {v2[0] - v1[0], v2[1] - v1[1], v2[2] - v1[2]};
+    GLfloat V[] = {v3[0] - v1[0], v3[1] - v1[1], v3[2] - v1[2]};
+
+    GLfloat x = U[1] * V[2] - U[2] * V[1];
+    GLfloat y = U[2] * V[0] - U[0] * V[2];
+    GLfloat z = U[0] * V[1] - U[1] * V[0];
+    //printf("[%f, ", x);
+    //printf("%f, ", y);
+    //printf("%f] \n", z);
+    
+    normales[i] = x;      normales[i+3] = x; normales[i+6] = x;
+    normales[i + 1] = y;  normales[i+4] = y; normales[i+7] = y;
+    normales[i + 2] = z;  normales[i+5] = z; normales[i+8] = z;
+    
+    printf("[%f, ", normales[i]);
+    printf("%f, ", normales[i + 1]);
+    printf("%f] \n", normales[i + 2]);
+  }
+
 }
--- spinningcube_withlight_fs.glsl
+++ spinningcube_withlight_fs.glsl
@@ -1,11 +1,5 @@
 #version 130
 
-out vec4 frag_col;
-
-in vec3 frag_3Dpos;
-in vec3 vs_normal;
-in vec2 TexCoords;
-
 struct Material {
   vec3 ambient;
   vec3 diffuse;
@@ -20,22 +14,32 @@ struct Light {
   vec3 specular;
 };
 
+out vec4 frag_col;
+
+in vec3 frag_3Dpos;
+in vec3 vs_normal;
+in vec2 TexCoords;
+
 uniform Material material;
 uniform Light light;
 uniform vec3 view_pos;
 
 void main() {
 
+  // Ambiente
   vec3 ambient = light.ambient * material.ambient;
+
+  // Difusión
   vec3 light_dir = normalize(light.position - frag_3Dpos);
-
   float diff = max(dot(vs_normal, light_dir), 0.0);
-  vec3 diffuse = light.diffuse * diff * material.diffuse;
+  vec3 diffuse = light.diffuse * (diff * material.diffuse);
 
+  // Especular
   vec3 view_dir = normalize(view_pos - frag_3Dpos);
   vec3 reflect_dir = reflect(-light_dir, vs_normal);
   float spec = pow(max(dot(view_dir, reflect_dir), 0.0), material.shininess);
-  vec3 specular = light.specular * spec * material.specular;
+  vec3 specular = light.specular * (spec * material.specular);
+
 
   vec3 result = ambient + diffuse + specular;
   frag_col = vec4(result, 1.0);
